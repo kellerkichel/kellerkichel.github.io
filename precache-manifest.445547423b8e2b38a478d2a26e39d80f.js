7b2d9514a0a6d455207c432f7232f7122f69861a
keller <kellerkichel@hotmail.com>
2019-11-12 16:32:36 -0300

    first test

---
+++ precache-manifest.445547423b8e2b38a478d2a26e39d80f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "611fb00ac9504fcdaeb850d907c2d52b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c76aaaac2918ada9c70",
+    "url": "/static/css/main.3d983770.chunk.css"
+  },
+  {
+    "revision": "c1fca502ee3d24469b42",
+    "url": "/static/js/2.626ff70a.chunk.js"
+  },
+  {
+    "revision": "5c76aaaac2918ada9c70",
+    "url": "/static/js/main.606d21ae.chunk.js"
+  },
+  {
+    "revision": "64a38abf639bee7e054b",
+    "url": "/static/js/runtime-main.eafd41a3.js"
+  }
+]);
